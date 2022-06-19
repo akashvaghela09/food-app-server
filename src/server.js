e767173fc6d73e53be905686335db5eb3d69bd6d
Akash Vaghela <akashvaghela09@gmail.com>
2022-06-19 13:27:56 +0530

    file updated

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ const  { connect } = require("./config/db")
 require("dotenv").config();
 const PORT = 2244
 
-console.log(process.env.DATABASE_URL)
-app.listen(process.env.PORT || PORT, async () => {
+app.listen(process.env.PORT || PORT, '0.0.0.0', async () => {
     await connect()
     console.log(`Listening on port ${PORT}`);
 })
